de5bc8f178ef0ae3bad9d996442a4703d96e3e91
JorgEdu1 <jorge.eduardo@alu.ufc.br>
2024-01-13 00:30:05 -0300

    finalizando a configuração do projeto

--- index.js
+++ index.js
@@ -7,3 +7,56 @@ const flash = require('express-flash');
 const app = express();
 
 const conn = require('./db/conn');
+
+//template engine
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+//request body parser
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+)
+
+//session
+app.use(
+    session({
+        name: "session",
+        secret: "nosso_secret",
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function () { },
+            path: require('path').join(require('os').tmpdir(), 'sessions'),
+        }),
+        cookie:{
+            secure: false,
+            maxAge: 3600000,
+            expires: new Date(Date.now() + 3600000),
+            httpOnly: true,
+        }
+    })
+)
+
+//flash messages
+app.use(flash());
+
+//public path
+app.use(express.static('public'));
+
+//set session to res
+app.use((req, res, next) => {
+    if(req.session.userid){
+        res.locals.session = req.session;
+    }
+    next();
+});
+
+conn.sync().then(() => {
+    app.listen(3000, () => {
+        console.log('Server running on port 3000');
+    });
+}).catch((err) => {
+    console.log(err);
+});
