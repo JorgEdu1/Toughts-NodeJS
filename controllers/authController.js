9f920fec4084bae6f7f6cfd55441c42d2f2c360d
JorgEdu1 <jorge.eduardo@alu.ufc.br>
2024-01-13 15:02:32 -0300

    criando sessao

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,5 +21,39 @@ module.exports = class AuthController {
 
             return
         }
+
+        //check if user already exists
+        const checkIfUserExists = await User.findOne({ where: { email: email } });
+
+        if (checkIfUserExists) {
+            req.flash('message', 'Email ja esta em uso, tente novamente!');
+            res.render('auth/register');
+
+            return
+        }
+
+        //create a password
+        const salt = bcrypt.genSaltSync(10);
+        const hashed = bcrypt.hashSync(password, salt);
+
+        const user = {
+            name,
+            email,
+            password: hashed
+        }
+
+        try {
+            const createdUser = await User.create(user);
+
+            //initialize session
+            req.session.userid = createdUser.id
+
+            req.flash('message', 'Usuario criado com sucesso!');
+            req.session.save(() => {
+                res.redirect('/');
+            });
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
